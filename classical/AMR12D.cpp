69ad527496849cbdd56adbadb665bc5860598a63
Lai Tuan <laituan245@kaist.ac.kr>
2015-08-12 21:00:29 +0900

    Added a solution to problem 'AMR12D - The mirror of Galadriel'

---
+++ classical/AMR12D.cpp
@@ -0,0 +1,26 @@
+// AMR12D - The Mirror of Galadriel
+// http://www.spoj.com/problems/AMR12D/
+// laituanksa245
+
+#include <iostream>
+#include <string>
+#include <map>
+using namespace std;
+
+int main () {
+  int t;
+  string tempStr;
+  bool isMagical;
+  scanf ("%d", &t);
+  for (int z = 0; z < t; z++) {
+    isMagical = true;
+    cin >> tempStr;
+    for (int i = 0; i < tempStr.length(); i++)
+      if (tempStr[i] != tempStr[tempStr.length()-1-i])
+        isMagical = false;
+    if (isMagical)
+      cout << "YES\n";
+    else
+      cout << "NO\n";
+  }
+}
